1aa16161fd00b029c045926772674449a5707ebd
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-11 20:33:12 -0600

    Add files via upload

---
+++ Ejercicio 1 - hola mundo/Hola mundo.cpp
@@ -0,0 +1,32 @@
+/*
+    Prop?sito: Calcular el promedio y los porcentajes de las ventas de tres d?as en una tienda.
+
+    Autor: Brian Josue Ramos Jeronimo
+    Fecha: 14 / 10 / 2021
+    Hora: 5:00 pm
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(int argc,char const *argv[] )
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+    string nombre;
+    
+    cout << "Digite su nombre " << endl;
+    cin >> nombre;
+    
+    cout << endl;
+    cout << "Mi nombre es " << nombre  << endl;
+    cout << endl;
+
+
+   return 0;
+}
+
