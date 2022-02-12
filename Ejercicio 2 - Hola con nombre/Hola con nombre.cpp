394cac5f9ea1dd3e8daba49dce1503231c50c792
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-11 20:33:33 -0600

    Add files via upload

---
+++ Ejercicio 2 - Hola con nombre/Hola con nombre.cpp
@@ -0,0 +1,27 @@
+/*
+    
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(int argc,char const *argv[] )
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+    int numeroEntero;
+    numeroEntero = 15;
+    
+    cout << "Entero: " << numeroEntero  << endl;
+    
+    numeroEntero = 18;
+    
+    cout << "Entero Modificado: " << numeroEntero  << endl;
+
+   return 0;
+}
+
