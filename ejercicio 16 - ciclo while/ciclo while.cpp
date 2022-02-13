ff979412dc3462cb39a168f69735fe59406550fb
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-12 20:25:18 -0600

    Add files via upload

---
+++ ejercicio 16 - ciclo while/ciclo while.cpp
@@ -0,0 +1,36 @@
+/*
+    Prop?sito: MOSTRAR LA NOTA OBTENIDA
+
+    Autor: Brian Josue Ramos Jeronimo
+    Fecha: 09 / 02 / 2022
+    Hora: 5:00 pm
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+   int a = 5;
+   int b = 2;
+   
+   while (a > b) {
+    cout << "Ingrese el valor de a : " << endl;
+    cin >> a;
+   
+   
+   
+   }
+ 
+   cout << endl;
+   cout << "saliste del ciclo : ";
+   
+   return 0;
+}
+
