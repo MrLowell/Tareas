0f207f91999fea224f8b3780628d16299ce76432
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-13 20:10:30 -0600

    Add files via upload

---
+++ ejercicio 18 - menu/menu.cpp
@@ -0,0 +1,64 @@
+/*
+    Prop?sito: MOSTRAR LA NOTA OBTENIDA
+
+    Autor: Brian Josue Ramos Jeronimo
+    Fecha: 09 / 02 / 2022
+    Hora: 5:00 pm
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+   int opcion = 0;
+   
+   
+   while (true)
+   { system ("cls");
+     cout << "****" << endl; 
+     cout << "menu" << endl; 
+     cout << "****" << endl; 
+   
+   
+     cout << endl; 
+     cout << "1 - Cafe y Granitas " << endl; 
+     cout << "2 - Reposteria" << endl; 
+     cout << "0 - Salir" << endl; 
+     
+     cout << "Ingrese una opcion del menu:";
+     cin >> opcion;
+     
+     if (opcion == 1) {
+     system ("cls");
+     cout << "Esta en el menu Cafe y Granitas:" << endl;
+	 system ("pause");
+	 }
+    
+    if (opcion == 2) {
+     system ("cls");
+     cout << "Esta en el menu de Reposteria:" << endl; 
+	 system ("pause");
+	 }
+    
+    if (opcion == 0) {
+       break;
+   }   
+	 
+	 
+	 
+   }
+   
+   cout << endl; 
+   cout << "Saliste del sistema:";
+ 
+   
+   return 0;
+}
+
