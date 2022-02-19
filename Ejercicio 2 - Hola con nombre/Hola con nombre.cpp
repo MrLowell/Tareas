1e63ab6e9acfb39b6f5163c7cf720a6caedda985
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-19 09:44:17 -0600

    Add files via upload

---
+++ Ejercicio 2 - Hola con nombre/Hola con nombre.cpp
@@ -0,0 +1,28 @@
+
+
+
+
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(int argc,char const *argv[] )
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+    string nombre;
+    
+    cout << "Digite su nombre " << endl;
+    cin >> nombre;
+    
+    cout << endl;
+    cout << "Mi nombre es " << nombre  << endl;
+    cout << endl;
+
+
+   return 0;
+}
+
