a7d6ed81bc00f35ef5bdf88809b62683a5c4961e
MrLowell <98554705+MrLowell@users.noreply.github.com>
2022-02-19 09:43:49 -0600

    Add files via upload

---
+++ Ejercicio 1 - hola mundo/Hola mundo.cpp
@@ -0,0 +1,24 @@
+/*
+    
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Spanish");
+    cout.precision(8);
+    
+    cout << "hola mundo " << endl;
+
+
+
+
+
+   return 0;
+}
+
